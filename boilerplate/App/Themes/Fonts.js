f3034950d6f7df2854ec5c928914a1eb310d91ca
Jimmy Bosse <jimmy@jobostudios.net>
2017-12-03 13:01:18 -0500

    Fix from @JadRizk

--- boilerplate/App/Themes/Fonts.js
+++ boilerplate/App/Themes/Fonts.js
@@ -1,7 +1,7 @@
 const type = {
-  base: 'Avenir-Book',
-  bold: 'Avenir-Black',
-  emphasis: 'HelveticaNeue-Italic'
+  base: (Platform.OS === 'ios' ? 'Helvetica Neue' : 'sans-serif'),
+  bold: (Platform.OS === 'ios' ? 'HelveticaNeue-Bold' : 'sans-serif-condensed'),
+  emphasis: (Platform.OS === 'ios' ? 'HelveticaNeue-Italic' : 'sans-serif')
 }
 
 const size = {
